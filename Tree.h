05456594b6e6c65c7416a3b39f15c4efe76b7fb6
Стилян Тодоров <72560983+BaLLeRZz@users.noreply.github.com>
2022-08-09 23:40:07 +0300

    Add files via upload

--- Tree.h
+++ Tree.h
@@ -10,23 +10,25 @@ private:
 	std::vector<Tree> children{};
 	std::vector<Tree> direct_children{};
 	void copy(const Tree& other);
-	bool insert_helper(const Tree& node, bool helper);
+	const bool insert_helper(const Tree& node, bool helper);
 	void remove_helper(const Tree& node, size_t count);
-	std::string print_helper(const std::vector<Tree> subtrees, std::string result, size_t count) const;
+	std::string print_helper(const std::vector<Tree>& subtrees, std::string result, size_t count) const;
+	const bool contains_helper(const std::vector<Tree>& subtrees, const std::vector<Tree>& other_subtrees);
 public:
 	Tree(const int value);
 	Tree(const Tree& other);
 	~Tree();
 	Tree& operator=(const Tree& other);
-	bool operator==(const Tree& other) const;
+	const bool operator==(const Tree& other) const;
 
 	const int get_value() const;
 	const std::vector<Tree> get_children() const;
 	const std::vector<Tree> get_direct_children() const;
-	bool insert(const int value);
-	bool insert(const Tree& node);
-	bool remove(const int value);
-    bool remove(const Tree& node);
+	const bool insert(const int value);
+	const bool insert(const Tree& node);
+	const bool remove(const int value);
+    const bool remove(const Tree& node);
+	const bool contains(const Tree& node);
 	void print() const;
 };
 
@@ -37,7 +39,7 @@ void Tree::copy(const Tree& other)
 	this->direct_children = other.direct_children;
 }
 
-bool Tree::insert_helper(const Tree& node, bool helper)
+const bool Tree::insert_helper(const Tree& node, bool helper)
 {
 	size_t size = this->direct_children.size();
 	for (size_t i = 0; i < size; i++)
@@ -69,7 +71,7 @@ void Tree::remove_helper(const Tree& node, size_t count)
 	this->children.erase(this->children.begin() + count);
 }
 
-std::string Tree::print_helper(const std::vector<Tree> subtrees, std::string result, size_t count) const
+std::string Tree::print_helper(const std::vector<Tree>& subtrees, std::string result, size_t count) const
 {
 	if (count == this->children.size())
 		return result;
@@ -104,6 +106,50 @@ std::string Tree::print_helper(const std::vector<Tree> subtrees, std::string res
 	return this->print_helper(lower_levels, result, count);
 }
 
+const bool Tree::contains_helper(const std::vector<Tree>& subtrees, const std::vector<Tree>& other_subtrees)
+{
+	if (subtrees.size() != other_subtrees.size())
+		return false;
+
+	size_t size = subtrees.size(), size_children{};
+	bool flag = true;
+	for (size_t i = 0; i < size; i++)
+	{
+		flag = false;
+		for (size_t j = 0; j < size; j++)
+		{
+			if (subtrees[i] == other_subtrees[j])
+			{
+				flag = true;
+				break;
+			}
+		}
+
+		if (!flag)
+			return false;
+	}
+
+	std::vector<Tree> lower_levels1{}, lower_levels2{};
+	for (size_t i = 0; i < size; i++)
+	{
+		size_children = subtrees[i].direct_children.size();
+		for (size_t j = 0; j < size_children; j++)
+			lower_levels1.push_back(subtrees[i].direct_children[j]);
+	}
+
+	for (size_t i = 0; i < size; i++)
+	{
+		size_children = other_subtrees[i].direct_children.size();
+		for (size_t j = 0; j < size_children; j++)
+			lower_levels2.push_back(other_subtrees[i].direct_children[j]);
+	}
+
+	if (other_subtrees.empty())
+		return true;
+
+	return this->contains_helper(lower_levels1, lower_levels2);
+}
+
 Tree::Tree(const int value)
 {
 	this->value = value;
@@ -127,7 +173,7 @@ Tree& Tree::operator=(const Tree& other)
 	return *this;
 }
 
-bool Tree::operator==(const Tree& other) const
+const bool Tree::operator==(const Tree& other) const
 {
 	if (this->value != other.value)
 		return false;
@@ -166,7 +212,7 @@ const std::vector<Tree> Tree::get_direct_children() const
 	return this->direct_children;
 }
 
-bool Tree::insert(const int value)
+const bool Tree::insert(const int value)
 {
 	for (Tree child : this->direct_children)
 		if (child.value == value)
@@ -178,7 +224,7 @@ bool Tree::insert(const int value)
 	return true;
 }
 
-bool Tree::insert(const Tree& node)
+const bool Tree::insert(const Tree& node)
 {
 	if (this->insert_helper(node, true))
 		return true;
@@ -186,7 +232,7 @@ bool Tree::insert(const Tree& node)
 	return false;
 }
 
-bool Tree::remove(const int value)
+const bool Tree::remove(const int value)
 {
 	size_t count{}, numbers{};
 	for (Tree child : this->children)
@@ -228,7 +274,7 @@ bool Tree::remove(const int value)
 	return true;
 }
 
-bool Tree::remove(const Tree& node)
+const bool Tree::remove(const Tree& node)
 {
 	size_t count{}, numbers{}, size = this->children.size();
 	for (Tree child : this->children)
@@ -264,6 +310,20 @@ bool Tree::remove(const Tree& node)
 
 		numbers--;
 	}
+	return false;
+}
+
+const bool Tree::contains(const Tree& node)
+{
+	for (Tree child : this->children)
+	{
+		if (child.value == node.value)
+		{
+			if (this->contains_helper(child.direct_children, node.direct_children))
+				return true;
+		}
+	}
+
 	return false;
 }
 
